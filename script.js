c7d09ff7f0fc38183dd64279420c52cf29475053
Still-Seeker <151577316+still-seeker@users.noreply.github.com>
2025-08-14 06:12:02 +0300

    Add unique id to all books

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const myLibrary = [];
 
 class Book {
     constructor(title, author, pages, category) {
+        this.id = crypto.randomUUID();
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -9,22 +10,25 @@ class Book {
     }
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, pages, category) {
+    const book = new Book(title, author, pages, category)
     myLibrary.push(book);
 }
+console.log(myLibrary)
 
-const book1 = new Book("To Kill a Mockingbird", "Haper lee", 700, "history");
-const book2 = new Book("The Seven Husbands of Evelyn Hugo", "Taylor Jenkins", 500, "Fiction");
-const book3 = new Book("The Alcheimist", "Paulo Coelho", 135, "Fiction");
-const book4 = new Book("The Fault in Our Stars", "John Green", 400, "Astronomy");
-const book5 = new Book("The Book Thief", "Markus Zusak", 545, "Fiction");
-const book6 = new Book("The Hunger Games", "Suzanne Collins", 605, "Literature");
-const book7 = new Book("The Midnight Library", "Matt Haig", 440, "Relationships");
-const book8 = new Book("The Perks of Being a Wallflower", "Stephan Chbosky", 811, "Literature");
-const book9 = new Book("The da Vinci code", "Dan Brown", 434, "Philosophy");
-const book10 = new Book("Harry Potter and the philosopher's stone", "J. K. Rowling", 699, "Fiction");
-const book11 = new Book("A little Life", "Hanya Yanagiraha", 239, "Relationships");
-const book12 = new Book("The Kite Runner", "Khaled Hosseini", 594, "Literature");
-const book13 = new Book("What if?: Serious scientific ANswers to Absurd Hypothetical Questions", "Randall Munroe", 565, "science");
-const book14 = new Book("Pride and Prejudice", "Jane Austen", 1000, "Relationships");
-const book15 = new Book("Twilight", "Stephenie Meyer", 480, "Fiction");
+addBookToLibrary("To Kill a Mockingbird", "Haper lee", 700, "history");
+addBookToLibrary("The Seven Husbands of Evelyn Hugo", "Taylor Jenkins", 500, "Fiction");
+addBookToLibrary("The Alcheimist", "Paulo Coelho", 135, "Fiction");
+addBookToLibrary("The Fault in Our Stars", "John Green", 400, "Astronomy");
+addBookToLibrary("The Book Thief", "Markus Zusak", 545, "Fiction");
+addBookToLibrary("The Hunger Games", "Suzanne Collins", 605, "Literature");
+addBookToLibrary("The Midnight Library", "Matt Haig", 440, "Relationships");
+addBookToLibrary("The Perks of Being a Wallflower", "Stephan Chbosky", 811, "Literature");
+addBookToLibrary("The da Vinci code", "Dan Brown", 434, "Philosophy");
+addBookToLibrary("Harry Potter and the philosopher's stone", "J. K. Rowling", 699, "Fiction");
+addBookToLibrary("A little Life", "Hanya Yanagiraha", 239, "Relationships");
+addBookToLibrary("The Kite Runner", "Khaled Hosseini", 594, "Literature");
+addBookToLibrary("What if?: Serious scientific ANswers to Absurd Hypothetical Questions", "Randall Munroe", 565, "science");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 1000, "Relationships");
+addBookToLibrary("Twilight", "Stephenie Meyer", 480, "Fiction");
+
